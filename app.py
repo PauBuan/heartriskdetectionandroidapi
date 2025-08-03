d978c843c685843933a10cce4c7e8817464c50cc
PauBuan <154594654+PauBuan@users.noreply.github.com>
2025-08-03 17:27:37 +0800

    Ui fixes and Prediction implementation
    shoot me

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from Controller.auth_controller import auth_bp
 from Controller.prediction_controller import prediction_bp
 from Controller.record_controller import record_bp
-from Controller.android_api_controller import android_api_bp
+from Controller.android_api_controller import api_bp
 from config.database import DatabaseConfig
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ CORS(app, resources={r"/api/android/*": {"origins": "*"}})
 app.register_blueprint(auth_bp)
 app.register_blueprint(prediction_bp)
 app.register_blueprint(record_bp)
-app.register_blueprint(android_api_bp, url_prefix='/api/android')
+app.register_blueprint(api_bp, url_prefix='/api/android')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
